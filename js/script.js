4504394325908d6b6fb6071a0ed6747fcbe6b50c
EC2 Default User <ec2-user@ip-172-31-94-207.ec2.internal>
2019-08-29 13:42:49 +0000

    added href to google markers and modal to private tours

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+document.getElementById('private').addEventListener('click', 
+  function(){
+      document.querySelector('.bg-modal').style.display ='flex';
+  });
+
+
 //initiate google map
 function initMap(){
   var options = {
@@ -9,36 +15,45 @@ var map = new google.maps.Map(document.getElementById('map'), options);
 //add markers details
 addMarker({
   coords:{lat:6.2956, lng:99.7228},
-  content:'<h6> Cenang Beach </h6>',
+  content:'<h6> Cenang Beach </h6>' + '<a href="http://www.langkawi-info.com/cenang/">'+
+            'http://www.langkawi-info.com/cenang/</a> '
 });
 addMarker({
   coords:{lat:6.4544, lng:99.8219},
-  content:'<h6> Tanjung Rhu </h6>'
+  content:'<h6> Tanjung Rhu </h6>' + '<a href="http://www.langkawi-info.com/tanjung-rhu/">'+
+            'http://www.langkawi-info.com/tanjung-rhu/</a> '
 });
 addMarker({
   coords:{lat:6.3711, lng:99.6717},
-  content:'<h6> Cable Car and Skybridge </h6>'
+  content:'<h6> Cable Car and Skybridge </h6>'+ '<a href="http://www.panoramalangkawi.com/">'+
+            'http://www.panoramalangkawi.com/</a> '
 });
 addMarker({
   coords:{lat:6.4030, lng:99.7127},
-  content:'<h6> Crocodile Adventureland </h6>'
+  content:'<h6> Crocodile Adventureland </h6>' + '<a href="http://www.crocodileadventureland.com/">'+
+            'http://www.crocodileadventureland.com/</a> '
 });
 addMarker({
   coords:{lat:6.4340, lng:99.7082},
-  content:'<h6> Temurun Waterfall </h6>'
+  content:'<h6> Temurun Waterfall </h6>' + '<a href="https://www.langkawi-insight.com/langkawi_0000ae.htm">'+
+            'https://www.langkawi-insight.com/langkawi_0000ae.htm</a> '
 });
 addMarker({
   coords:{lat:6.4059, lng:99.8597},
-  content:'<h6> Geopark Mangrove </h6>'
+  content:'<h6> Geopark Mangrove </h6>' + '<a href="https://langkawimangrove.com/">'+
+            'https://langkawimangrove.com/</a> '
 });
 addMarker({
   coords:{lat:6.3878, lng:99.8597},
-  content:'<h6> Wildlife Park </h6>'
+  content:'<h6> Wildlife Park </h6>' + '<a href="https://langkawiwildlifepark.com/">'+
+            'https://langkawiwildlifepark.com/</a> '
 });
 addMarker({
   coords:{lat:6.3831, lng:99.6741},
-  content:'<h6> Seven Wells Waterfall </h6>'
+  content:'<h6> Seven Wells Waterfall </h6>' + '<a href="https://www.langkawi-insight.com/langkawi_0000ac.htm">'+
+            'https://www.langkawi-insight.com/langkawi_0000ac.htm</a> '
 });
+
 
 //function for addMarker
 var activeWindow;
