b7cb88bf3b3e67e224b3ba181b0b7b66d55cffc5
EC2 Default User <ec2-user@ip-172-31-94-207.ec2.internal>
2019-09-03 10:27:28 +0000

    customised card decks and adding photos to infowindows

--- js/script.js
+++ js/script.js
@@ -10,71 +10,67 @@ document.getElementById('private').addEventListener('click',
   });
 
 
-//initiate google map
+//initiate google map, centering at Langkawi
 function initMap(){
-  var options = {
+  var centerMap = {
     zoom:11,
     center:{lat:6.3500, lng:99.8000}
 };
-var map = new google.maps.Map(document.getElementById('map'), options);
+var map = new google.maps.Map(document.getElementById('map'), centerMap);
 
-//add markers details
-addMarker({
-  coords:{lat:6.2956, lng:99.7228},
+//details of multiple markers
+var multipleMarkers = [
+  {location:{lat:6.2956, lng:99.7228},
   content:'<h6> Cenang Beach </h6>' + '<a href="http://www.langkawi-info.com/cenang/">'+
-            'http://www.langkawi-info.com/cenang/</a> '
-});
-addMarker({
-  coords:{lat:6.4544, lng:99.8219},
+            'http://www.langkawi-info.com/cenang/</a> '+ '<img src="images/cenang.jpg">'},
+  {location:{lat:6.4544, lng:99.8219},
   content:'<h6> Tanjung Rhu </h6>' + '<a href="http://www.langkawi-info.com/tanjung-rhu/">'+
-            'http://www.langkawi-info.com/tanjung-rhu/</a> '
-});
-addMarker({
-  coords:{lat:6.3711, lng:99.6717},
+            'http://www.langkawi-info.com/tanjung-rhu/</a> '+ '<img src="images/beach.jpg">',},
+  {location:{lat:6.3711, lng:99.6717},
   content:'<h6> Cable Car and Skybridge </h6>'+ '<a href="http://www.panoramalangkawi.com/">'+
-            'http://www.panoramalangkawi.com/</a> '
-});
-addMarker({
-  coords:{lat:6.4030, lng:99.7127},
+            'http://www.panoramalangkawi.com/</a> ' + '<img src="images/skybridge.jpg">'},
+  {location:{lat:6.4030, lng:99.7127},
   content:'<h6> Crocodile Adventureland </h6>' + '<a href="http://www.crocodileadventureland.com/">'+
-            'http://www.crocodileadventureland.com/</a> '
-});
-addMarker({
-  coords:{lat:6.4340, lng:99.7082},
+            'http://www.crocodileadventureland.com/</a> '+'<img src="images/crocfarm.jpg">' },
+  {location:{lat:6.4340, lng:99.7082},
   content:'<h6> Temurun Waterfall </h6>' + '<a href="https://www.langkawi-insight.com/langkawi_0000ae.htm">'+
-            'https://www.langkawi-insight.com/langkawi_0000ae.htm</a> '
-});
-addMarker({
-  coords:{lat:6.4059, lng:99.8597},
+            'https://www.langkawi-insight.com/langkawi_0000ae.htm</a>' + '<img src="images/temurun.jpg">'},
+  {location:{lat:6.4059, lng:99.8597},
   content:'<h6> Geopark Mangrove </h6>' + '<a href="https://langkawimangrove.com/">'+
-            'https://langkawimangrove.com/</a> '
-});
-addMarker({
-  coords:{lat:6.3878, lng:99.8597},
+            'https://langkawimangrove.com/</a> '+ '<img src="images/mangrovepark.jpg">'},
+  {location:{lat:6.3878, lng:99.8597},
   content:'<h6> Wildlife Park </h6>' + '<a href="https://langkawiwildlifepark.com/">'+
-            'https://langkawiwildlifepark.com/</a> '
-});
-addMarker({
-  coords:{lat:6.3831, lng:99.6741},
+            'https://langkawiwildlifepark.com/</a> '+ '<img src="images/hornbill.jpg">'},
+  {location:{lat:6.3831, lng:99.6741},
   content:'<h6> Seven Wells Waterfall </h6>' + '<a href="https://www.langkawi-insight.com/langkawi_0000ac.htm">'+
-            'https://www.langkawi-insight.com/langkawi_0000ac.htm</a> '
-});
+            'https://www.langkawi-insight.com/langkawi_0000ac.htm</a> '+ '<img src="images/waterfall.JPG">'},
+  {location:{lat:6.2877, lng:99.7286},
+  content:'<h6>  Langkawi Underwater World </h6>' + '<a href="https://www.langkawi-insight.com/langkawi_0000ac.htm">'+
+            'https://www.langkawi-insight.com/langkawi_0000ac.htm</a> ' + '<img src="images/Dugong.jpeg">'}
 
+  ];
+//loops through details of multiple markers  
+  for (var i =0; i < multipleMarkers.length; i++){
+    addMarker(multipleMarkers[i]);
+  }
 
-//function for addMarker
+//function for addMarker to allow multiple markers on the map
 var activeWindow;
-function addMarker(props){
+
+function addMarker(coords){
   var marker = new google.maps.Marker({
-  position:props.coords,
+  position:coords.location,
   map:map,
   animation:google.maps.Animation.DROP,
   });
 
-  if (props.content){
+  if (coords.content){
     var infowindow = new google.maps.InfoWindow ({
-      content:props.content
+      content:coords.content
   });
+
 //calling function with opening infowindow on click and closing infowindow on next click
+
   marker.addListener('click',function(){
     if (activeWindow){activeWindow.close();}
   infowindow.open(map, marker);
@@ -84,3 +80,4 @@ function addMarker(props){
   }
   }
 }
+
