cdb6b36d24a634a852d5441f4a2b3bb53dae6749
EC2 Default User <ec2-user@ip-172-31-94-207.ec2.internal>
2019-09-04 08:40:54 +0000

    d3 chart with csv

--- js/script.js
+++ js/script.js
@@ -66,7 +66,8 @@ function addMarker(coords){
 
   if (coords.content){
     var infowindow = new google.maps.InfoWindow ({
-      content:coords.content
+      content:coords.content,
+      maxWidth:300
   });
 
 //calling function with opening infowindow on click and closing infowindow on next click
@@ -81,3 +82,78 @@ function addMarker(coords){
   }
 }
 
+//TEMPERATURE GRAPH
+// set the dimensions and margins of the graph
+var margin = {top: 30, right: 30, bottom: 70, left: 60},
+    width = 350 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#chart")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+  
+
+// Parse the Data
+d3.csv("./langkawi_temperature.csv", function(data) {
+
+// X axis
+var x = d3.scaleBand()
+  .range([ 0, width ])
+  .domain(data.map(function(d) { return d.Months; }))
+  .padding(0.2);
+svg.append("g")
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(x))
+  .selectAll("text")
+    .attr("transform", "translate(-10,0)rotate(-45)")
+    .style("text-anchor", "end")
+    
+svg.append("text")             
+      .attr("transform",
+            "translate(" + (width/2) + " ," + 
+                           (height + margin.top + 40) + ")")
+      .style("text-anchor", "middle")
+      .text("Months");
+// Add Y axis
+var y = d3.scaleLinear()
+  .domain([0, 40])
+  .range([ height, 0]);
+
+svg.append("g")
+    .call(d3.axisLeft(y));
+    
+svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left)
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text(" Average Temperatures (Degree Celcius)");      
+
+// Bars
+svg.selectAll("mybar")
+  .data(data)
+  .enter()
+  .append("rect")
+    .attr("x", function(d) { return x(d.Months); })
+    .attr("y", function(d) { return y(d.Temperatures); })
+    .attr("width", x.bandwidth())
+    .attr("height", function(d) { return height - y(d.Temperatures); })
+    .attr("fill", "#17a2ba")
+    .attr("height", function(d) { return height - y(0); })
+    .attr("y", function(d) { return y(0); })
+
+// Animation
+svg.selectAll("rect")
+  .transition()
+  .duration(400)
+  .attr("y", function(d) { return y(d.Temperatures); })
+  .attr("height", function(d) { return height - y(d.Temperatures); })
+  .delay(function(d,i){console.log(i) ; return(i*120)})
+
+})
