68d4d395fc59046701bec167cd7a69d78e9f701f
EC2 Default User <ec2-user@ip-172-31-94-207.ec2.internal>
2019-09-11 10:23:19 +0000

    changing the size of the map

--- js/script.js
+++ js/script.js
@@ -9,79 +9,6 @@ document.getElementById('private').addEventListener('click',
       document.querySelector('.bg-modal').style.display ='none';
   });
 
-/*
-//initiate google map, centering at Langkawi
-function initMap(){
-  var centerMap = {
-    zoom:11,
-    center:{lat:6.3500, lng:99.8000}
-};
-var map = new google.maps.Map(document.getElementById('map'), centerMap);
-
-//details of multiple markers
-var multipleMarkers = [
-  {location:{lat:6.2956, lng:99.7228},
-  content:'<h6> Cenang Beach </h6>' + '<a href="http://www.langkawi-info.com/cenang/">'+
-            'http://www.langkawi-info.com/cenang/</a> '+ '<img src="images/cenang.jpg">'},
-  {location:{lat:6.4544, lng:99.8219},
-  content:'<h6> Tanjung Rhu </h6>' + '<a href="http://www.langkawi-info.com/tanjung-rhu/">'+
-            'http://www.langkawi-info.com/tanjung-rhu/</a> '+ '<img src="images/beach.jpg">',},
-  {location:{lat:6.3711, lng:99.6717},
-  content:'<h6> Cable Car and Skybridge </h6>'+ '<a href="http://www.panoramalangkawi.com/">'+
-            'http://www.panoramalangkawi.com/</a> ' + '<img src="images/skybridge.jpg">'},
-  {location:{lat:6.4030, lng:99.7127},
-  content:'<h6> Crocodile Adventureland </h6>' + '<a href="http://www.crocodileadventureland.com/">'+
-            'http://www.crocodileadventureland.com/</a> '+'<img src="images/crocfarm.jpg">' },
-  {location:{lat:6.4340, lng:99.7082},
-  content:'<h6> Temurun Waterfall </h6>' + '<a href="https://www.langkawi-insight.com/langkawi_0000ae.htm">'+
-            'https://www.langkawi-insight.com/langkawi_0000ae.htm</a>' + '<img src="images/temurun.jpg">'},
-  {location:{lat:6.4059, lng:99.8597},
-  content:'<h6> Geopark Mangrove </h6>' + '<a href="https://langkawimangrove.com/">'+
-            'https://langkawimangrove.com/</a> '+ '<img src="images/mangrovepark.jpg">'},
-  {location:{lat:6.3878, lng:99.8597},
-  content:'<h6> Wildlife Park </h6>' + '<a href="https://langkawiwildlifepark.com/">'+
-            'https://langkawiwildlifepark.com/</a> '+ '<img src="images/hornbill.jpg">'},
-  {location:{lat:6.3831, lng:99.6741},
-  content:'<h6> Seven Wells Waterfall </h6>' + '<a href="https://www.langkawi-insight.com/langkawi_0000ac.htm">'+
-            'https://www.langkawi-insight.com/langkawi_0000ac.htm</a> '+ '<img src="images/waterfall.JPG">'},
-  {location:{lat:6.2877, lng:99.7286},
-  content:'<h6>  Langkawi Underwater World </h6>' + '<a href="https://www.langkawi-insight.com/langkawi_0000ac.htm">'+
-            'https://www.langkawi-insight.com/langkawi_0000ac.htm</a> ' + '<img src="images/Dugong.jpeg">'}
-
-  ];
-//loops through details of multiple markers  
-  for (var i =0; i < multipleMarkers.length; i++){
-    addMarker(multipleMarkers[i]);
-  }
-
-//function for addMarker to allow multiple markers on the map
-var activeWindow;
-
-function addMarker(coords){
-  var marker = new google.maps.Marker({
-  position:coords.location,
-  map:map,
-  animation:google.maps.Animation.DROP,
-  });
-
-  if (coords.content){
-    var infowindow = new google.maps.InfoWindow ({
-      content:coords.content,
-      maxWidth:300
-  });
-
-//calling function with opening infowindow on click and closing infowindow on next click
-
-  marker.addListener('click',function(){
-    if (activeWindow){activeWindow.close();}
-  infowindow.open(map, marker);
-  activeWindow = infowindow;
-  });
-
-  }
-  }
-}
-*/
 
 var map, places, infoWindow;
       var markers = [];
@@ -93,7 +20,7 @@ var map, places, infoWindow;
       var countries = {
         'mys': {
           center: {lat:4.210484 , lng:101.97576600000002},
-          zoom: 7
+          zoom: 8
         },
        
       };
@@ -305,8 +232,8 @@ var map, places, infoWindow;
 //TEMPERATURE GRAPH
 // set the dimensions and margins of the graph
 var margin = {top: 20, right: 20, bottom: 60, left: 50},
-    width = 260 - margin.left - margin.right,
-    height = 260 - margin.top - margin.bottom;
+    width = 280 - margin.left - margin.right,
+    height = 290 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#chart")
@@ -361,7 +288,7 @@ svg.append("text")
       .attr("x",0 - (height / 2))
       .attr("dy", "1em")
       .style("text-anchor", "middle")
-      .text(" Average Temperatures (Degree Celcius)");      
+      .text(" Average Temperatures (ºC)");      
 
 // Bars
 svg.selectAll("mybar")
