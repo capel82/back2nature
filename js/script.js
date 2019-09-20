11139269393d5d1b02232d88267f8eb26eed3723
EC2 Default User <ec2-user@ip-172-31-94-207.ec2.internal>
2019-09-20 12:05:41 +0000

    customised infowindow for map

--- js/script.js
+++ js/script.js
@@ -9,228 +9,79 @@ document.getElementById('private').addEventListener('click',
       document.querySelector('.bg-modal').style.display ='none';
   });
 
+//initiate google map, centering at Langkawi
+function initMap(){
+  var centerMap = {
+    zoom:11,
+    center:{lat:6.3500, lng:99.8000}
+};
+var map = new google.maps.Map(document.getElementById('map'), centerMap);
 
-var map, places, infoWindow;
-      var markers = [];
-      var autocomplete;
-      var countryRestrict = {'country': 'mys'};
-      var MARKER_PATH = 'https://developers.google.com/maps/documentation/javascript/images/marker_green';
-      var hostnameRegexp = new RegExp('^https?://.+?/');
+//details of multiple markers
+var multipleMarkers = [
+  {location:{lat:6.2956, lng:99.7228},
+  content:'<h5> Cenang Beach </h5>' + '<p class="text-center"><a class="btn btn-light btn-sm" href="http://www.langkawi-info.com/cenang/" role="button">More Info</a></p>'+ ''+
+             '<img class="my-3" src="images/cenang.jpg">'},
+  {location:{lat:6.4544, lng:99.8219},
+  content:'<h5> Tanjung Rhu </h5>' + '<p class="text-center"><a class="btn btn-light btn-sm" href="http://www.langkawi-info.com/tanjung-rhu/" role="button">More Info</a></p>'+''+
+            '<img src="images/beach.jpg">'},
+  {location:{lat:6.3711, lng:99.6717},
+  content:'<h5> Cable Car and Skybridge </h5>'+ '<p class="text-center"><a class="btn btn-light btn-sm" href="http://www.panoramalangkawi.com/" role="button">More Info</a></p>'+''+
+           '<img src="images/skybridge.jpg">'},
+  {location:{lat:6.4030, lng:99.7127},
+  content:'<h5> Crocodile Adventureland </h5>' + '<p class="text-center"><a class="btn btn-light btn-sm" href="http://www.crocodileadventureland.com/"role="button">More Info</a></p>'+ ''+
+            '<img src="images/crocfarm.jpg">' },
+  {location:{lat:6.4340, lng:99.7082},
+  content:'<h5> Temurun Waterfall </h5>' + '<p class="text-center"><a class="btn btn-light btn-sm" href="https://www.langkawi-insight.com/langkawi_0000ae.htm" role="button">More Info</a></p>'+ ''+
+            '<img src="images/temurun.jpg">'},
+  {location:{lat:6.4059, lng:99.8597},
+  content:'<h5> Geopark Mangrove </h5>' + '<p class="text-center"><a class="btn btn-light btn-sm" href="https://langkawimangrove.com/" role="button">More Info</a></p>'+ ''+
+            '<img src="images/mangrovepark.jpg">'},
+  {location:{lat:6.3878, lng:99.8597},
+  content:'<h5> Wildlife Park </h5>' + '<p class="text-center"><a class="btn btn-light btn-sm" href="https://langkawiwildlifepark.com/" role="button">More Info</a></p>'+ ''+
+            '<img src="images/hornbill.jpg">'},
+  {location:{lat:6.3831, lng:99.6741},
+  content:'<h5> Seven Wells Waterfall </h5>' + '<p class="text-center"><a class="btn btn-light btn-sm" href="https://www.langkawi-insight.com/langkawi_0000ac.htm" role="button">More Info</a></p>'+ ''+
+            '<img src="images/waterfall.JPG">'},
+  {location:{lat:6.2877, lng:99.7286},
+  content:'<h5>  Langkawi Underwater World </h5>' + '<p class="text-center"><a class="btn btn-light btn-sm" href="https://www.langkawi-insight.com/langkawi_0000ac.htm" role="button">More Info</a></p>'+ ''+
+            '<img src="images/Dugong.jpeg">'}
 
-      var countries = {
-        'mys': {
-          center: {lat:4.210484 , lng:101.97576600000002},
-          zoom: 8
-        },
-       
-      };
+  ];
+//loops through details of multiple markers  
+  for (var i =0; i < multipleMarkers.length; i++){
+    addMarker(multipleMarkers[i]);
+  }
 
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          zoom: countries['mys'].zoom,
-          center: countries['mys'].center,
-          mapTypeControl: false,
-          panControl: false,
-          zoomControl: false,
-          streetViewControl: false
-        });
+//function for addMarker to allow multiple markers on the map
+var activeWindow;
 
-        infoWindow = new google.maps.InfoWindow({
-          content: document.getElementById('info-content')
-        });
+function addMarker(coords){
+  var marker = new google.maps.Marker({
+  position:coords.location,
+  map:map,
+  animation:google.maps.Animation.DROP,
+  });
 
-        // Create the autocomplete object and associate it with the UI input control.
-        // Restrict the search to the default country, and to place type "cities".
-        /*autocomplete = new google.maps.places.Autocomplete(
-             @type {!HTMLInputElement}  (
-                document.getElementById('autocomplete')), {
-              types: ['(cities)'],
-              componentRestrictions: countryRestrict
-            });
-            */
-        places = new google.maps.places.PlacesService(map);
-        search();
+  if (coords.content){
+    var infowindow = new google.maps.InfoWindow ({
+      content:coords.content,
+      maxWidth:300
+  });
 
-       // autocomplete.addListener('place_changed', onPlaceChanged);
+//calling function with opening infowindow on click and closing infowindow on next click
 
-        // Add a DOM event listener to react when the user selects a country.
-        /*document.getElementById('country').addEventListener(
-            'change', setAutocompleteCountry);*/
-      }
+  marker.addListener('click',function(){
+    if (activeWindow){activeWindow.close();}
+  infowindow.open(map, marker);
+  activeWindow = infowindow;
+  });
 
-      // When the user selects a city, get the place details for the city and
-      // zoom the map in on the city.
-     /* function onPlaceChanged() {
-        var place = autocomplete.getPlace();
-        if (place.geometry) {
-          map.panTo(place.geometry.location);
-          map.setZoom(11);
-          search();
-        } else {
-          document.getElementById('autocomplete').placeholder = 'Enter a city';
-        }
-      }*/
+  }
+  }
+}
 
-      // Search for hotels in the selected city, within the viewport of the map.
-      function search() {
-        var search = {
-          bounds: map.getBounds(),
-          types: ['lodging']
-        };
 
-        places.nearbySearch(search, function(results, status) {
-          if (status === google.maps.places.PlacesServiceStatus.OK) {
-            clearResults();
-            clearMarkers();
-            // Create a marker for each hotel found, and
-            // assign a letter of the alphabetic to each marker icon.
-            for (var i = 0; i < results.length; i++) {
-              var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
-              var markerIcon = MARKER_PATH + markerLetter + '.png';
-              // Use marker animation to drop the icons incrementally on the map.
-              markers[i] = new google.maps.Marker({
-                position: results[i].geometry.location,
-                animation: google.maps.Animation.DROP,
-                icon: markerIcon
-              });
-              // If the user clicks a hotel marker, show the details of that hotel
-              // in an info window.
-              markers[i].placeResult = results[i];
-              google.maps.event.addListener(markers[i], 'click', showInfoWindow);
-              setTimeout(dropMarker(i), i * 100);
-              addResult(results[i], i);
-            }
-          }
-        });
-      }
-
-      function clearMarkers() {
-        for (var i = 0; i < markers.length; i++) {
-          if (markers[i]) {
-            markers[i].setMap(null);
-          }
-        }
-        markers = [];
-      }
-
-      // Set the country restriction based on user input.
-      // Also center and zoom the map on the given country.
-      function setAutocompleteCountry() {
-        var country = document.getElementById('country').value;
-        if (country == 'all') {
-          autocomplete.setComponentRestrictions({'country': []});
-          map.setCenter({lat: 15, lng: 0});
-          map.setZoom(2);
-        } else {
-          autocomplete.setComponentRestrictions({'country': country});
-          map.setCenter(countries[country].center);
-          map.setZoom(countries[country].zoom);
-        }
-        clearResults();
-        clearMarkers();
-      }
-
-      function dropMarker(i) {
-        return function() {
-          markers[i].setMap(map);
-        };
-      }
-
-      function addResult(result, i) {
-        var results = document.getElementById('results');
-        var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
-        var markerIcon = MARKER_PATH + markerLetter + '.png';
-
-        var tr = document.createElement('tr');
-        tr.style.backgroundColor = (i % 2 === 0 ? '#F0F0F0' : '#FFFFFF');
-        tr.onclick = function() {
-          google.maps.event.trigger(markers[i], 'click');
-        };
-
-        var iconTd = document.createElement('td');
-        var nameTd = document.createElement('td');
-        var icon = document.createElement('img');
-        icon.src = markerIcon;
-        icon.setAttribute('class', 'placeIcon');
-        icon.setAttribute('className', 'placeIcon');
-        var name = document.createTextNode(result.name);
-        iconTd.appendChild(icon);
-        nameTd.appendChild(name);
-        tr.appendChild(iconTd);
-        tr.appendChild(nameTd);
-        results.appendChild(tr);
-      }
-
-      function clearResults() {
-        var results = document.getElementById('results');
-        while (results.childNodes[0]) {
-          results.removeChild(results.childNodes[0]);
-        }
-      }
-
-      // Get the place details for a hotel. Show the information in an info window,
-      // anchored on the marker for the hotel that the user selected.
-      function showInfoWindow() {
-        var marker = this;
-        places.getDetails({placeId: marker.placeResult.place_id},
-            function(place, status) {
-              if (status !== google.maps.places.PlacesServiceStatus.OK) {
-                return;
-              }
-              infoWindow.open(map, marker);
-              buildIWContent(place);
-            });
-      }
-
-      // Load the place information into the HTML elements used by the info window.
-      function buildIWContent(place) {
-        document.getElementById('iw-icon').innerHTML = '<img class="hotelIcon" ' +
-            'src="' + place.icon + '"/>';
-        document.getElementById('iw-url').innerHTML = '<b><a href="' + place.url +
-            '">' + place.name + '</a></b>';
-        document.getElementById('iw-address').textContent = place.vicinity;
-
-        if (place.formatted_phone_number) {
-          document.getElementById('iw-phone-row').style.display = '';
-          document.getElementById('iw-phone').textContent =
-              place.formatted_phone_number;
-        } else {
-          document.getElementById('iw-phone-row').style.display = 'none';
-        }
-
-        // Assign a five-star rating to the hotel, using a black star ('&#10029;')
-        // to indicate the rating the hotel has earned, and a white star ('&#10025;')
-        // for the rating points not achieved.
-        if (place.rating) {
-          var ratingHtml = '';
-          for (var i = 0; i < 5; i++) {
-            if (place.rating < (i + 0.5)) {
-              ratingHtml += '&#10025;';
-            } else {
-              ratingHtml += '&#10029;';
-            }
-          document.getElementById('iw-rating-row').style.display = '';
-          document.getElementById('iw-rating').innerHTML = ratingHtml;
-          }
-        } else {
-          document.getElementById('iw-rating-row').style.display = 'none';
-        }
-
-        // The regexp isolates the first part of the URL (domain plus subdomain)
-        // to give a short URL for displaying in the info window.
-        if (place.website) {
-          var fullUrl = place.website;
-          var website = hostnameRegexp.exec(place.website);
-          if (website === null) {
-            website = 'http://' + place.website + '/';
-            fullUrl = website;
-          }
-          document.getElementById('iw-website-row').style.display = '';
-          document.getElementById('iw-website').textContent = website;
-        } else {
-          document.getElementById('iw-website-row').style.display = 'none';
-        }
-      }
 //TEMPERATURE GRAPH
 // set the dimensions and margins of the graph
 var margin = {top: 20, right: 20, bottom: 60, left: 50},
